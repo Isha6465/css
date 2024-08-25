14c0af47652e9cf4f09523cab59f88785c6a0998
isha ishtiaq <ishaishtiaq9@gmail.com>
2024-08-25 18:16:21 +0500

    excuse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,23 @@
 import React from 'react'
 
-function page() {
+const page = () => {
   return (
-    <div><figure className="md:flex bg-blue-200 rounded-xl p-8 md:p-0 dark:bg-black-700">
-    <img className="w-21 h-21 object:cover object:center md: ms-10 rounded-full mx-auto" src="/1.jpg" alt="" width="300" height="100"/>
-    <div className="pt-6 md:p-5 text-center md:text-left space-y-4">
+    <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+    <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/1.jpg" alt="" width="384" height="512"/>
+    <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
-        <p className=" font-medium text-slate-700 dark:text-blue-1000 mx-28 pt-14">
-          
-        "I am currently pursuing a Bachelor's degree at Khwaja Fareed University of Engineering and Information Technology. In addition to my studies, I am actively engaged in a Generative AI course and am also enrolled in a Web Development course to further enhance my skills in these areas."
+        <p className="text-lg font-medium">
+          “Tailwind CSS is the only framework that I've seen scale
+          on large teams. It’s easy to customize, adapts to any design,
+          and the build size is tiny.”
         </p>
       </blockquote>
-      <figcaption className="font-medium text-sm mx-28">
-        <div className="text-blue-700 dark:text-blue-900">
-        Isha Ishtiaq
+      <figcaption className="font-medium">
+        <div className="text-sky-500 dark:text-sky-400">
+          Sarah Dayan
         </div>
-        <div className="text-blue-700 dark:text-blue-900">
-       BSCS,KFUEIT
+        <div className="text-slate-700 dark:text-slate-500">
+          Staff Engineer, Algolia
         </div>
       </figcaption>
     </div>
