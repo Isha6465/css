dd2a922f9571bd809afefb1ae2655d70b1dfc6ff
isha ishtiaq <ishaishtiaq9@gmail.com>
2024-08-25 17:56:39 +0500

    upgradation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 function page() {
   return (
     <div><figure className="md:flex bg-blue-200 rounded-xl p-8 md:p-0 dark:bg-black-700">
-    <img className="w-21 h-21 object:cover object:center md:  md:rounded-none rounded-full mx-auto" src="/1.jpg" alt="" width="300" height="100"/>
+    <img className="w-21 h-21 object:cover object:center md: ms-10 rounded-full mx-auto" src="/1.jpg" alt="" width="300" height="100"/>
     <div className="pt-6 md:p-5 text-center md:text-left space-y-4">
       <blockquote>
-        <p className=" font-medium text-blue-900 dark:text-blue-1000">
+        <p className=" font-medium text-slate-700 dark:text-blue-1000 mx-28 pt-14">
           
-        I am currently pursuing a Bachelor's degree at Khwaja Fareed University of Engineering and Information Technology. In addition to my studies, I am actively engaged in a Generative AI course and am also enrolled in a Web Development course to further enhance my skills in these areas.
+        "I am currently pursuing a Bachelor's degree at Khwaja Fareed University of Engineering and Information Technology. In addition to my studies, I am actively engaged in a Generative AI course and am also enrolled in a Web Development course to further enhance my skills in these areas."
         </p>
       </blockquote>
-      <figcaption className="font-medium text-lg">
-        <div className="text-black-800 dark:text-black-400">
+      <figcaption className="font-medium text-sm mx-28">
+        <div className="text-blue-700 dark:text-blue-900">
         Isha Ishtiaq
         </div>
         <div className="text-blue-700 dark:text-blue-900">
